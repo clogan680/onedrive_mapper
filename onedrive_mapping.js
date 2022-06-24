39bfcf85f0a64208fbfcdbae98e32c4352a221ea
unknown <clogan680@gmail.com>
2022-06-24 16:15:50 -0700

    Working on JSON construction

--- onedrive_mapping.js
+++ onedrive_mapping.js
@@ -4,7 +4,6 @@ const compileUserList = require('./helpers/compileUserList');
 const getAllDrives = require('./helpers/getAllDrives');
 const getRootItems = require('./helpers/getRootItems');
 const getChildItems = require('./helpers/getChildItems');
-const getChildrenData = require('./helpers/getchildrenData');
 const getItemPermissions = require('./helpers/getItemPermissions');
 
 
@@ -23,41 +22,51 @@ async function getDrives() {
             if (drives.data != undefined) {
                 let driveRoot = await getRootItems(allUsers[i].id, token)
                 let getChildren = await getChildItems(drives.data.value[0].id, driveRoot.data.id, token)
-                console.log(allUsers[i].Email, '------------------------------')
 
                 for (let y = 0; y < getChildren.data.value.length; y++) {
                     que.push(getChildren.data.value[y])
                 }
                 while (que.length > 0) {
                     for (let x = 0; x < que.length; x++) {
-                        // console.log(que[x])
 
                         let getPermissions = await getItemPermissions(drives.data.value[0].id, que[x].id, token)
-                        console.log(que[x].name, 'NAME OF ITEM ------------------------------------------------------------------')
-                        console.log
+                       
+                        for (let y = 0; y < getPermissions.data.value.length; y++) {
+                            let role = getPermissions.data.value[y].roles
+                            if (role[0] == 'owner') {
+                                let usersV2 = getPermissions.data.value[y].grantedToV2
+                                
+                                console.log(allUsers[i].Email, '------------------------------')
+                                console.log(que[x].parentReference.path, '------------ PATH')
+                                console.log(que[x].name, '-----------ITEM NAME ')
+                                console.log(role[0])
+                                console.log(Array.isArray(usersV2))
+                            } else {
+                                let usersV2 = getPermissions.data.value[y].grantedToIdentitiesV2
+                                
+                                console.log(allUsers[i].Email, '------------------------------')
+                                console.log(que[x].parentReference.path, '------------ PATH')
+                                console.log(que[x].name, '-----------ITEM NAME ')
+                                console.log(role[0])
+                                console.log(Array.isArray(usersV2))
+                            }
+                        }
                         var folder
                         try {
                             folder = que[x].folder.childCount
-                            // console.log(folder, 'FOLDER-----------')
                         } catch (error) {
                             folder = 0
                         }
                         if (folder > 0) {
-
-                            console.log(folder, 'FOLDER HAS CHILDREN-------------')
+                            // console.log(folder, 'FOLDER HAS CHILDREN-------------')
                             let getChildren = await getChildItems(drives.data.value[0].id, que[x].id, token)
                             for (let z = 0; z < getChildren.data.value.length; z++) {
                                 que.push(getChildren.data.value[z])
                             }
-
                         }
                         que.shift(que[x])
                     }
                 }
-                // let childrenData = await getChildrenData(drives.data.value[0].id, getChildren.data.value, token)
-                // console.log(getChildren.data.value)
-                // let getPermissions = await getItemPermissions(drives.data.value[0].id, driveRoot.data.id, token)
-                // console.log(getPermissions.data.value)
             }
         } catch (error) { }
     };
